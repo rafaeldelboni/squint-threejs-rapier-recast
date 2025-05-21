a1aba3e14df6b74061bebac9493e05609eaa2b7c
Rafael Delboni <rafadelboni@gmail.com>
2025-05-21 00:05:51 -0300

    feat: add agent and sync with physics

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,14 +1,15 @@
 (ns app.core
   (:require
-   ["@recast-navigation/core" :rename {init init-recast}]
-   ["@recast-navigation/three" :refer [threeToSoloNavMesh NavMeshHelper]]
    ["@dimforge/rapier3d-compat" :as rapier]
+   ["@recast-navigation/core" :rename {init init-recast} :refer [Crowd]]
+   ["@recast-navigation/three" :refer [NavMeshHelper threeToSoloNavMesh]]
    ["three/addons/controls/OrbitControls.js" :refer [OrbitControls]]
    ["three/addons/environments/RoomEnvironment.js" :refer [RoomEnvironment]]
    ["three/addons/loaders/GLTFLoader.js" :refer [GLTFLoader]]
    [three :refer [ACESFilmicToneMapping Box3 BufferAttribute BufferGeometry
-                  Color LineBasicMaterial LineSegments PerspectiveCamera
-                  PMREMGenerator Scene Vector3 Quaternion Euler WebGLRenderer]]))
+                  CapsuleGeometry Color Clock Euler LineBasicMaterial LineSegments
+                  Mesh MeshBasicMaterial PerspectiveCamera PMREMGenerator
+                  Quaternion Scene Vector3 WebGLRenderer]]))
 
 (defn- prepare-container! [id]
   (let [container (js/document.getElementById id)]
@@ -24,7 +25,8 @@
         renderer (new WebGLRenderer)
         environment (new RoomEnvironment)
         pmremGenerator (new PMREMGenerator renderer)
-        generated-env (.fromScene pmremGenerator environment)]
+        generated-env (.fromScene pmremGenerator environment)
+        clock (new Clock)]
     (set! (.-background scene) (new Color 0xbbbbbb))
     (set! (.-environment scene) (.-texture generated-env))
     (set! (.-toneMapping renderer) ACESFilmicToneMapping)
@@ -34,6 +36,7 @@
     (.set (-> camera .-position) 0 5 -10)
     (new OrbitControls camera (.-domElement renderer))
     {:scene scene
+     :clock clock
      :camera camera
      :renderer renderer}))
 
@@ -62,6 +65,10 @@
                                      (.-w collider-rotation)))
     (.updateMatrix mesh)))
 
+(defn sync-agent-rigid-body [^js agent ^js rigid-body]
+  (let [{x "x" y "y" z "z"} (js->clj (.position agent))]
+    (.setNextKinematicTranslation rigid-body #js {:x x :y (+ y 0.5) :z z})))
+
 (defn debug-render [^js lines ^js world]
   (let [buffers (.debugRender world)]
     (.setAttribute (.-geometry lines) "position" (new BufferAttribute (.-vertices buffers) 3))
@@ -76,10 +83,11 @@
 (defn static-cube [^js world ^js mesh]
   (-> mesh .-geometry .computeBoundingBox)
   (let [reset-mesh (calc-reset-mesh mesh)
-        ^js bounding-box (.getSize (.setFromObject (new Box3) reset-mesh) (new Vector3))
-        x-size (/ (.-x bounding-box) 2)
-        y-size (/ (.-y bounding-box) 2)
-        z-size (/ (.-z bounding-box) 2)
+        ^js bounding-box (.setFromObject (new Box3) reset-mesh)
+        ^js bounding-box-size (.getSize bounding-box (new Vector3))
+        x-size (/ (.-x bounding-box-size) 2)
+        y-size (/ (.-y bounding-box-size) 2)
+        z-size (/ (.-z bounding-box-size) 2)
         x (-> mesh .-position .-x)
         y (-> mesh .-position .-y)
         z (-> mesh .-position .-z)
@@ -132,6 +140,32 @@
     (sync-mesh-collider mesh collider)
     {:mesh mesh :collider collider :rigid-body rigid-body}))
 
+(defn rigid-capsule-with-geometry [^js scene ^js world ^js crowd]
+  (let [radius 0.5
+        height 1
+        geometry (new CapsuleGeometry radius height 10 20)
+        material (new MeshBasicMaterial radius height 10 20)
+        mesh (new Mesh geometry material)
+        {:keys [x y z] :as position} {:x -3 :y 1 :z 3}
+        ^js rigid-body-desc (-> (-> rapier .-RigidBodyDesc .kinematicPositionBased)
+                                (.setTranslation  x y z)
+                                (.lockRotations) ; prevent rotations along all axes.
+                                (.enabledRotations false false false))
+        ^js rigid-body (.createRigidBody world rigid-body-desc)
+        ^js collider-desc ((-> rapier .-ColliderDesc .-capsule) (/ height 2) radius)
+        ^js collider (.createCollider world
+                                      (-> collider-desc
+                                          (.setRestitution  0.0))
+                                      rigid-body)
+        ^js agent (.addAgent crowd (clj->js position) #js {:radius (* radius 2)
+                                                           :height height
+                                                           :maxAcceleration 10.0
+                                                           :maxSpeed 6.0})]
+    (.add scene mesh)
+    (sync-mesh-collider mesh collider)
+    (.requestMoveTarget ^js agent #js {:x 3 :y 0.3 :z -3})
+    {:mesh mesh :collider collider :rigid-body rigid-body :agent agent}))
+
 (defn children->physics-objects [^js meshes ^js world]
   (doall (keep (fn [^js mesh]
                  (let [extras (.-userData mesh)
@@ -145,20 +179,20 @@
                meshes)))
 
 (def recast-config
-  #js {:borderSize 0,
-       :tileSize 0,
-       :cs 0.2,
-       :ch 0.2,
-       :walkableSlopeAngle 60,
-       :walkableHeight 2,
-       :walkableClimb 2,
-       :walkableRadius 1,
-       :maxEdgeLen 12,
-       :maxSimplificationError 1.3,
-       :minRegionArea 8,
-       :mergeRegionArea 20,
-       :maxVertsPerPoly 6,
-       :detailSampleDist 6,
+  #js {:borderSize 0
+       :tileSize 0
+       :cs 0.2
+       :ch 0.2
+       :walkableSlopeAngle 60
+       :walkableHeight 2
+       :walkableClimb 2
+       :walkableRadius 2
+       :maxEdgeLen 12
+       :maxSimplificationError 1.3
+       :minRegionArea 8
+       :mergeRegionArea 20
+       :maxVertsPerPoly 6
+       :detailSampleDist 6
        :detailSampleMaxError 1})
 
 (defn children->navmesh
@@ -173,7 +207,8 @@
 (defn app [_]
   (let [{:keys [^Scene scene
                 ^PerspectiveCamera camera
-                ^WebGLRenderer renderer]} system
+                ^WebGLRenderer renderer
+                ^Clock clock]} system
 
         gravity #js {:x 0.0 :y -9.81 :z 0.0}
         ^js world (new (.-World rapier) gravity)
@@ -188,16 +223,23 @@
                  (.add scene (.-scene gltf))
                  (-> gltf .-scene (.updateMatrixWorld true))
                  (let [children (-> gltf .-scene .-children)
-                       physics-objects (children->physics-objects children world)
                        navmesh (children->navmesh children)
+                       crowd  (new Crowd (.-navMesh navmesh) #js {:maxAgents 1 :maxAgentRadius 0.5})
+                       agent-object (rigid-capsule-with-geometry scene world crowd)
+                       agents-objects [agent-object]
+                       physics-objects (into agents-objects (children->physics-objects children world))
                        navMeshHelper (new NavMeshHelper (.-navMesh navmesh))]
                    (.add scene navMeshHelper) ; debug navimesh
                    (.setAnimationLoop renderer (fn ^js []
-                                                 (doseq [{:keys [mesh collider]} physics-objects]
-                                                   (sync-mesh-collider mesh collider))
-                                                 (debug-render debug-lines world)
-                                                 (.step world)
-                                                 (.render renderer scene camera)))))))))
+                                                 (let [delta (.getDelta clock)]
+                                                   (doseq [{:keys [rigid-body agent]} agents-objects]
+                                                     (sync-agent-rigid-body agent rigid-body))
+                                                   (doseq [{:keys [mesh collider]} physics-objects]
+                                                     (sync-mesh-collider mesh collider))
+                                                   (debug-render debug-lines world)
+                                                   (.update crowd delta)
+                                                   (.step world)
+                                                   (.render renderer scene camera))))))))))
 
 (-> (.all js/Promise [(.init rapier) (init-recast)])
     (.then app))
--- todo.md
+++ todo.md
@@ -2,7 +2,7 @@
  - [x] Basic integration: npm instal
  - [x] Generate navmesh based on gltf "static" meshes
  - [x] Fine tune navmesh generation using better recast options
- - [ ] Scafold agent with physics capsule
- - [ ] Sync physics capsule with threejs geometry
- - [ ] Basic movements of agent from rapier
- - [ ] Make agent move between two points in navmesh
+ - [x] Scafold agent with physics capsule
+ - [x] Sync physics capsule with threejs geometry
+ - [x] Basic movements of agent from rapier
+ - [x] Make agent move between two points in navmesh
